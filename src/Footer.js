4acb2dfa50e76cda43d409289a60ccd4a310c419
Vraj Limbachiya <88069540+Vraj0109@users.noreply.github.com>
2022-11-27 15:20:45 +0530

    Create Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
             {/* <!-- Copyright --> */}
             <div className="text-white mb-3 mb-md-0">
-                Copyright © 2020. All rights reserved.
+                Copyright © 2020. All rights reserved..
             </div>
         </div>
     )
